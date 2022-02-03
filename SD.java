dbf536efdc3e4a75a8c2a90e0497b2ec7e4cd656
Max Rivett <max.rivett@ucc.on.ca>
2022-02-02 19:06:43 -0800

    Added tests and fails

--- SD.java
+++ SD.java
@@ -1,3 +1,8 @@
 public class SD { 
 
+    public static String concat(String s1, String s2) {
+        String temp = s1 + s2;
+        return temp;
+    }
+
 }
---
+++ SDTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SDTest {
+    
+    @Test
+    public void concatTest() {
+        assertEquals("CSE15L", SD.concat("CSE", "12"));
+    }
+
+}
